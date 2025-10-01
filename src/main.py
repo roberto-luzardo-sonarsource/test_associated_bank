014f59a9abd7eeef81795a00c831af0a026b23f4
Roberto Luzardo <roberto.luzardo@sonarsource.com>
2025-10-01 13:04:00 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,14 @@ def lambda_handler(event, context):
     # Placeholder Code
 
     print('Placeholder code')
+    
+    var = "test value"
 
+    print("Event Contents")
+    print(event)
+    # print("Context Contents")
+    # print(context)
+    
     return {
 
         'statusCode': 200,
