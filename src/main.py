07a3092b57215e731186d50440bc6671222e5eee
Roberto Luzardo <roberto.luzardo@sonarsource.com>
2025-10-01 12:30:48 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def lambda_handler(event, context):
+
+    # Placeholder Code
+
+    print('Placeholder code')
+
+    return {
+
+        'statusCode': 200,
+
+        'body': json.dumps('Hello from Lambda!')
+
+    }
